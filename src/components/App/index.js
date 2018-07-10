344ceb1e72ee9a05fd52237dacb2a4236d2d04a2
Chris Jarvis <jrvscm@gmail.com>
2018-07-10 13:28:58 -0600

    initial

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <fieldset style={{maxWidth: 300}}>
+            <legend>Pdf Parser</legend>
+
+        </fieldset>
+        <button style={{marginTop: 15, marginBottom: 15}}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Form/index.js
@@ -0,0 +1,7 @@
+        
+        <fieldset style={{maxWidth: 300}}>
+            <legend>Pdf Parser</legend>
+
+        </fieldset>
+        <button style={{marginTop: 15, marginBottom: 15}}>Submit</button>
+        </form>
